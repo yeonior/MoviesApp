8215f5ca64e16760778ad02bb95e32ef60d0fa2b
Rus <de.mr44@gmail.com>
2022-05-13 10:46:42 +0500

    Implemented a table view at HomeViewController

--- MoviesApp/Controllers/HomeViewController.swift
+++ MoviesApp/Controllers/HomeViewController.swift
@@ -9,8 +9,67 @@ import UIKit
 
 final class HomeViewController: UIViewController {
     
+    // MARK: - Subviews
+    private let homeFeedTable: UITableView = {
+        $0.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return $0
+    }(UITableView())
+    
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureUI()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        configureHomeFeedTable()
+    }
+    
+    // MARK: - Methods
+    private func configureUI() {
         view.backgroundColor = .systemBackground
+        
+        homeFeedTable.dataSource = self
+        homeFeedTable.delegate = self
+        view.addSubview(homeFeedTable)
+    }
+    
+    private func configureHomeFeedTable() {
+        homeFeedTable.frame = view.bounds
+    }
+}
+
+// MARK: - UITableViewDataSource
+extension HomeViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        4
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "Home"
+        cell.backgroundColor = .systemGray4
+        
+        return cell
+    }
+}
+
+// MARK: - UITableViewDelegate
+extension HomeViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        40
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        UIView()
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        200
     }
 }
