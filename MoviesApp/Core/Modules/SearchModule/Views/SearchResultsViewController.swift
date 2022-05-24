5038eb5a563dd4c32e4feb08f1e755ebe60cca04
Rus <de.mr44@gmail.com>
2022-05-24 20:30:55 +0500

    Added SearchResultsViewController

---
+++ MoviesApp/Core/Modules/SearchModule/Views/SearchResultsViewController.swift
@@ -0,0 +1,22 @@
+//
+//  SearchResultsViewController.swift
+//  MoviesApp
+//
+//  Created by Ruslan on 24.05.2022.
+//
+
+import UIKit
+
+final class SearchResultsViewController: UIViewController {
+    
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+    }
+    
+    // MARK: - Private methods
+    private func configureUI() {
+        view.backgroundColor = .red
+    }
+}
