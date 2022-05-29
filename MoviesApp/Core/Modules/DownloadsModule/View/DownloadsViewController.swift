67bd4b8f92a4d84825e4a05ed1b9ed05c0a92b14
Rus <de.mr44@gmail.com>
2022-05-29 20:22:00 +0500

    Updated DownloadsViewController

--- MoviesApp/Core/Modules/DownloadsModule/View/DownloadsViewController.swift
+++ MoviesApp/Core/Modules/DownloadsModule/View/DownloadsViewController.swift
@@ -9,8 +9,54 @@ import UIKit
 
 final class DownloadsViewController: UIViewController {
     
+    // MARK: - Properties
+    private var titles: [TitleItem] = []
+    
+    // MARK: - Subviews
+    private let downloadsTableView: UITableView = {
+        let table = UITableView()
+        table.register(TitleTableViewCell.self, forCellReuseIdentifier: TitleTableViewCell.identifier)
+        return table
+    }()
+    
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureUI()
+    }
+    
+    // MARK: - Private methods
+    private func configureUI() {
+        title = "Downloads"
         view.backgroundColor = .systemBackground
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.navigationItem.largeTitleDisplayMode = .always
+        navigationController?.navigationBar.tintColor = .label
+        
+        downloadsTableView.dataSource = self
+        downloadsTableView.delegate = self
+    }
+}
+
+// MARK: - UITableViewDataSource
+extension DownloadsViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        titles.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: TitleTableViewCell.identifier, for: indexPath) as? TitleTableViewCell else { return UITableViewCell() }
+        
+        let title = titles[indexPath.row]
+        cell.configure(with: TitleViewModel(titleName: (title.originalTitle ?? title.originalName) ?? "N/A", posterURL: title.posterPath ?? ""))
+        
+        return cell
+    }
+}
+
+// MARK: - UITableViewDelegate
+extension DownloadsViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        180
     }
 }
